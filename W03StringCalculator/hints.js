68ed61e0433be7bc84262d4762d100d7cef8fde9
Renzo Barrios <w3renzo@gmail.com>
2021-01-06 13:26:25 -0300

    Initial commit :tada:

---
+++ W03StringCalculator/hints.js
@@ -0,0 +1,32 @@
+/* 
+Hint 1:
+    To parse the equation, you can use the "split" method 
+    on the equation string to break it up 
+    into an array of its components
+
+    Like this:
+        let equationString = "3 * 5"
+        equationString.split(" ") -> ["3", "*", "5"]
+    
+Hint 2:
+    A switch statement or a chain of if/else statements 
+    can help you evaluate which operation to do 
+    based on the equation's operator: +, -, *, or / 
+    
+Hint 3: 
+    You can use the built in `parseInt()` to parse a string
+    containing an integer value into an integer
+    
+    Like this:
+        let myValue = "42"
+        parseInt(myValue) -> 42
+
+Hint 4:
+    Row-based Flexbox:
+        Horizontal Alignment: `justify-content`
+        Vertical Alignment: `align-items`
+    
+    Column-based Flexbox
+        Horizontal Alignment: `align-items`
+        Vertical Alignment: `justify-content`
+*/
